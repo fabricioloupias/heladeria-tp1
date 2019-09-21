3fe93d89c3456017c5f4f66667a7f191facfeae9
Fabricio Loupias <loupias.fabri@gmail.com>
2019-09-21 17:47:42 -0300

    Heladeria taller 1

--- Heladeria/src/clases/Normal.java
+++ Heladeria/src/clases/Normal.java
@@ -3,20 +3,11 @@ package clases;
 class Normal extends Gusto {
 	
 	private static final double RECARGO = 1;
-	private TipoGusto tipoGusto;
-	private int id;
-
+	
 	public Normal(TipoGusto tipoGusto, int id) {
 		super(tipoGusto, id);
 	}
 
-	public TipoGusto getTipoGusto() {
-		return tipoGusto;
-	}
-	
-	public int getId() {
-		return id;
-	}
 	
 	@Override
 	public double calcularRecargo(double precio){
